b804cafd13b51fdb18b1bfffff8a90bc4bbad6cd
tkcwebdev <tkcwebdev@gmail.com>
2020-09-03 15:59:41 -0700

    First commit showing off new logo and tagline

---
+++ app/src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import SplashScreen from '../SplashScreen/SplashScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <SplashScreen/>
+    </div>
+  );
+}
+
+export default App;
