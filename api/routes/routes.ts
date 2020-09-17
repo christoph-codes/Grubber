40c89fae90ed8ffdfed69bda645f5f9052be95ff
Tech314 <kyle.magdales@yahoo.com>
2020-09-16 20:22:33 -0700

    added route for authorize

--- api/routes/routes.ts
+++ api/routes/routes.ts
@@ -1,8 +1,10 @@
 
 import { Router } from 'express';
 import { healthCheck } from '../api/healthCheckApi/healthCheckApi';
+import { authRoutes } from './authRoutes';
 
 export const apiRoutes = (router: Router) => {
     router.all('/healthcheck', healthCheck);
+    router.apply(authRoutes);
     return router;
 };
