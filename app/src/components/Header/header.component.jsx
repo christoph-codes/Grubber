11d323391e9187c1f572888afb409969ccb221a2
Markus Realica <markus.developer2@gmail.com>
2020-09-19 09:15:46 -0700

    Add Logo to the header-component

--- app/src/components/Header/header.component.jsx
+++ app/src/components/Header/header.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './header.styles.scss';
 
+import logo from '../../assets/grubber_logo.svg';
+
 const Header = () => (
     <div className='header'>
+        <div className='logo-container'>
+            <img src={logo} className='logo' alt='Grubber is the future.'/>
+        </div>
         <div className='options'>
             <div className='option'>
                 HOME
