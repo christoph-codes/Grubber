9b9ecd6e4560f9b627299e0cc10dc8ae454a741f
Markus Realica <markus.developer2@gmail.com>
2020-09-19 09:22:00 -0700

    Add Links from react-router-dom to allow navigation from the header

--- app/src/components/Header/header.component.jsx
+++ app/src/components/Header/header.component.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import './header.styles.scss';
 
+import { Link } from 'react-router-dom';
+
 import logo from '../../assets/grubber_logo.svg';
 
 const Header = () => (
     <div className='header'>
-        <div className='logo-container'>
+        <Link className='logo-container' to='/'>
             <img src={logo} className='logo' alt='Grubber is the future.'/>
-        </div>
+        </Link>
         <div className='options'>
-            <div className='option'>
+            <Link className='option' to='/'>
                 HOME
-            </div>
-            <div className='option'>
+            </Link>
+            <Link className='option' to='/login'>
                 LOGIN
-            </div>
-            <div className='option'>
+            </Link>
+            <Link className='option' to='/create-account'>
                 CREATE ACCOUNT
-            </div>
+            </Link>
         </div>
     </div>
 )
