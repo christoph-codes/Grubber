eea85997da1b06b9854e201538fc4da9e627eb2f
Christopher Jones <tkcwebdev@gmail.com>
2020-09-19 09:47:39 -0700

    Removed Markus header component from this branch

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -2,33 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
 
-import Header from '../Header/header.component';
+// import Header from '../Header/header.component';
 
 import SplashScreen from '../SplashScreen/SplashScreen';
 import CreateAccount from '../pages/CreateAccount/CreateAccount';
 
 function App() {
-<<<<<<< HEAD
 	return (
 		<Router>
 			<div className='App'>
-				<Route path='/' exact component={SplashScreen} />
+        {/* Placeholder for header */}
+        <div className="page-container">
+        <Route path='/' exact component={SplashScreen} />
 				<Route path='/create-account' component={CreateAccount} />
+        </div>
+        {/* Placeholder for footer */}
 			</div>
 		</Router>
 	);
-=======
-  return (
-    <Router>
-      <Header/>
-      <div className="App">
-            <Route path="/" exact component={SplashScreen} />
-            <Route path="/create-account" component={CreateAccount} />
-          </div>
-    </Router>
-    
-  );
->>>>>>> cd0b36d49f45201f4f752c324b75751341a092e1
 }
 
 export default App;
