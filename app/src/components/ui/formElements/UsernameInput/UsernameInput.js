6363a7218c1acb89859b86ceaee79fb3c310e90c
tkcwebdev <tkcwebdev@gmail.com>
2020-09-24 10:44:40 -0700

    Fixed username self-validation WIP

--- app/src/components/ui/formElements/UsernameInput/UsernameInput.js
+++ app/src/components/ui/formElements/UsernameInput/UsernameInput.js
@@ -3,44 +3,28 @@ import { InputGroup, FormControl } from 'react-bootstrap';
 import './UsernameInput.scss'
 
 export default function UsernameInput({ placeholder, handler, feedback}) {
-    const [value,setValue] = useState('')
+    const [value,setValue] = useState('');
 
     const sendValue = (e) => {
         let val = e.target.value.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
         setValue(val)
-        // if(val.length > 0) {
-        //     let regex = /^[a-z0-9_-]{6,15}$/
-        //     if(regex.test(val)) {
-        //         // TODO: Check to see whether the value exists in the database already
-        //         feedback('')
-        //     } else {
-        //         feedback('Your username may include _ and - must be between 6 to 16 characters.')
-        //     }
-        // } else {
-        //     feedback('You must enter a unique usernamee.')
-        // }
-        
-        // handler(val);
-        console.log(value)
+        if(val.length > 0) {
+            let regex = /^[a-z0-9.]{6,16}$/
+            if(regex.test(val)) {
+                    setValue(val)
+                    feedback('')
+            } else {
+                feedback('Your unique username must be between 6 to 16 characters. It may include dashes or underscores.')
+            }
+        } else {
+            feedback('You must enter a unique username.')
+        }
+
     }
     useEffect(() => {
-        let val = e.target.value.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-        setValue(val)
-        // if(val.length > 0) {
-        //     let regex = /^[a-z0-9_-]{6,15}$/
-        //     if(regex.test(val)) {
-        //         // TODO: Check to see whether the value exists in the database already
-        //         feedback('')
-        //     } else {
-        //         feedback('Your username may include _ and - must be between 6 to 16 characters.')
-        //     }
-        // } else {
-        //     feedback('You must enter a unique usernamee.')
-        // }
-        
-        // handler(val);
-        console.log(value)
-    }, [value])
+        handler(value);
+    })
+
 	return (
 		<Fragment>
 			<InputGroup className='UsernameInput' size='lg'>
