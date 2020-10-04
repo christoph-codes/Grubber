cb66952f2b803e9cfc36316903d7161d7e3d16de
Markus Realica <markus.developer2@gmail.com>
2020-10-04 15:24:17 -0700

    Use react-bootstrap nav/navbar to create header and allow for mobile navigation

--- app/src/components/Header/header.component.jsx
+++ app/src/components/Header/header.component.jsx
@@ -1,34 +1,36 @@
 import React from 'react';
 import './header.styles.scss';
 
+import { Navbar, Nav } from 'react-bootstrap'
+
 import { NavLink } from 'react-router-dom';
 
 import logo from '../../assets/grubber_logo.svg';
 
 const Header = () => (
-    <div className='header'>
-        <NavLink className='logo-container' to='/'>
-            <img src={logo} className='logo' alt='Grubber is the future.'/>
-        </NavLink>
-        <div className='options'>
+    <Navbar collapseOnSelect className="header" bg="light" expand="lg">
+        <Navbar.Brand className="logo-container" href="/">
+            <img src={logo} className="logo" alt="Grubber is the future."/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto options">
             <NavLink 
-                className='option'
+                className="option"
                 activeStyle={{
                     color: "#DBA74C"
                 }}
-                exact to='/'>
-                Home
-            </NavLink>
+                exact
+                to="/">Home</NavLink>
             <NavLink 
-                className='option'
+                className="option" 
                 activeStyle={{
                     color: "#DBA74C"
-                }}
-                to='/create-account'>
-                Create Account
-            </NavLink>
-        </div>
-    </div>
+                }} 
+                to="/create-account">Create Account</NavLink>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
 )
 
 export default Header;
